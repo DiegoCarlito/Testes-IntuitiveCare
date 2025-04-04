df5e4e581d4762dc6189fe11b6aa51612f882ce2
DiegoCarlito <diego.carlito01@gmail.com>
2025-04-04 13:11:27 -0300

    feat: conectar frontend com o servidor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Busca de Operadoras Ativas</h1>
+    <input v-model="query" placeholder="Digite o nome da operadora" />
+    <button @click="buscarOperadoras">Buscar</button>
+    <ul v-if="operadoras.length">
+      <li v-for="operadora in operadoras" :key="operadora.registro_ans">
+        <p>{{ operadora.razao_social }} ({{ operadora.nome_fantasia }})</p>
+      </li>
+    </ul>
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      query: '',
+      operadoras: [],
+      errorMessage: ''
+    };
+  },
+  methods: {
+    async buscarOperadoras() {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/api/buscar?q=${this.query}`);
+        if (!response.ok) {
+          throw new Error('Erro na busca');
+        }
+        const data = await response.json();
+        this.operadoras = data;
+        this.errorMessage = '';
+      } catch (error) {
+        this.errorMessage = 'Erro ao buscar as operadoras. Tente novamente.';
+        this.operadoras = [];
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+input {
+  padding: 8px;
+  margin-right: 10px;
+}
+
+button {
+  padding: 8px;
+}
+
+ul {
+  margin-top: 20px;
 }
 </style>
